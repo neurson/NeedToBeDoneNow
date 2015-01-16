2f7dbe9e2f0d6739e23d134318d680f6671fd52e
neurson <yan.pearson@outlook.com>
2015-01-15 22:03:57 -0500

    Add 'createdOn' property for list schema.

--- models/list.js
+++ models/list.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose");
 
 var ListSchema = mongoose.Schema({
 	name: { type: String, required: true },
-	owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+	owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+	createdOn: { type: Date, required: true, default: Date.now()}
 });
 
 module.exports = mongoose.model('List', ListSchema);
